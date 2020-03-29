5375224e2ff4e0ab2dddcc0b7a2f85d842dac849
Paolo Santori <code@paolosantori.it>
2020-03-29 22:27:30 +0200

    Server initial import

---
+++ server/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    redistHost: process.env.REDIS_HOST,
+    redistPort: process.env.REDIS_PORT,
+    pgHost: process.env.PG_HOST,
+    pgUser: process.env.PG_USER,
+    pgPassword: process.env.PG_PASSWORD,
+    pgDatabase: process.env.PG_DATABASE,
+    pgPort: process.env.PG_PORT
+}
