a96d411cf31acc8f2f99da87eafa41583a92d53a
Paolo Santori <code@paolosantori.it>
2020-03-29 22:30:02 +0200

    No need to use components

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import OtherPage from './OtherPage';
 import Fib from './Fib';
 
-class App extends Component {
-  render() {
+function App() {
   return (
     <Router> 
       <div className="App">
         <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
+          <img src={logo} className="App-logo" alt="logo" />
         </header>
         <br />
-        <div>
-            <Link to="/">Home</Link> - <Link to="/otherpage">Other Page</Link>
-        </div>
+        <Link to="/">Home</Link> - <Link to="/otherpage">Other page</Link>
         <br />
-        <div>
-            <Route exact path="/" component={Fib} />
-            <Route path="/otherpage" component={OtherPage} />
-        </div>
+        <br />
+        <Route exact path="/" component={Fib} />
+        <Route path="/otherpage" component={OtherPage} />
       </div>
     </Router> 
   );
-  }
 }
 
 export default App;
